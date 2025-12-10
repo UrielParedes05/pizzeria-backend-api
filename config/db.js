9c663dd54f0603faaaf606d86190fad7670f75fc
urielparedes05 <urielparedes2005@gmail.com>
2025-12-10 01:22:43 -0500

    fix: Forzar conexión a DB a IPv4 (Solución a ENETUNREACH)

--- config/db.js
+++ config/db.js
@@ -11,6 +11,14 @@ const sequelize = new Sequelize(
     dialect: process.env.DB_DIALECT,
     logging: false,
     timezone: '-05:00',
+    // === SOLUCIÓN: FORZAR IPv4 ===
+    dialectOptions: {
+      host: process.env.DB_HOST,
+      ssl: false, // Desactiva SSL por si acaso (Supabase no lo requiere por defecto)
+      // La opción más importante: le dice al driver 'pg' que solo use IPv4
+      family: 4, 
+    },
+    // =============================
   }
 );
 
